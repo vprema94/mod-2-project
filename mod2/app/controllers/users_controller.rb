bea6ede217b6e3cc24f7339215cff10729128e76
Blair <blairacarroll@gmail.com>
2019-01-22 17:49:10 -0500

    start of user

--- mod2/app/controllers/users_controller.rb
+++ mod2/app/controllers/users_controller.rb
@@ -1,2 +1,51 @@
 class UsersController < ApplicationController
+  username
+  password
+  email
+  image
+  first_name
+  last_name
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password, :email, :image, :first_name, :last_name)
+  end
+
 end
