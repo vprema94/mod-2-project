c054d5cbadbcf507458a6b3671a25a8262980848
Blair <blairacarroll@gmail.com>
2019-01-23 13:54:50 -0500

    blair outfits and occasion

--- mod2/app/models/outfit.rb
+++ mod2/app/models/outfit.rb
@@ -3,4 +3,7 @@ class Outfit < ApplicationRecord
   has_many :pieces, through: :clothing_outfits
   belongs_to :occasion
   belongs_to :user
+
+  validates :rating, length: {maximum: 5}
+
 end
--- mod2/app/models/user.rb
+++ mod2/app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
   has_many :pieces
   has_many :outfits
+
+  validates :username, uniqueness: true
+  validates :email, uniqueness: true
+  validates :password, length: {in: 6..20}
+
+
 end
