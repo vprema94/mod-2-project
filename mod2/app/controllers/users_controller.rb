59df8724db9540bc81820ff05e7697ca3da95a63
Vanessa Prema <vanessabprema@gmail.com>
2019-04-03 14:35:33 -0400

    fixed sign up

--- mod2/app/controllers/users_controller.rb
+++ mod2/app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    byebug
     if @user.valid?
       @user.save
       session[:user_id] = @user.id
--- mod2/app/models/user.rb
+++ mod2/app/models/user.rb
@@ -6,6 +6,5 @@ class User < ApplicationRecord
 
   validates :username, uniqueness: true
   validates :email, uniqueness: true
-  validates :password, length: {in: 6..20}
 
 end
